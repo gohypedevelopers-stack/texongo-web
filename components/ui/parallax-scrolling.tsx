6e99d7e5ef92cb29613af49698dbb4a9533dda12
subhan khan <sk1623969@gmail.com>
2026-04-09 15:36:55 +0530

    agian update

--- components/ui/parallax-scrolling.tsx
+++ components/ui/parallax-scrolling.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import styles from '@/app/page.module.css';
 
 export function ParallaxComponent() {
